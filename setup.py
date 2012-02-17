77475e382e7e4fadf68459b996ec2d754d984a96
Sebastien Estienne <sebastien.estienne@gmail.com>
2012-02-17 18:34:49 +0100

    increase version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='picomongo',
-      version='0.4',
+      version='0.5',
       description='Ultimate MongoDB Object Document Mapper',
       author='Dailymotion IT Team',
       author_email='contact@dmcloud.net',
